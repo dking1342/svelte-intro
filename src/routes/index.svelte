7ce6bf105e66374ba516a01299462cceafa0c243
David <leanmachine42@yahoo.com>
2022-06-08 12:44:17 -0500

    worked with basic features, state, functions, routing, routes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
+<script>
+  let count = 0;
+
+  const increment = () => (count += 1);
+  const decrement = () => (count -= 1);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="/about">about</a> route
+</p>
+<h3>Number: {count}</h3>
+<button on:click={increment}>increment</button>
+<button on:click={decrement}>decrement</button>
+
+<style>
+  p {
+    font-size: 2rem;
+  }
+</style>
