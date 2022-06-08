c5861ed48aa7867d6431c8f2fd41db918e1bc923
David <leanmachine42@yahoo.com>
2022-06-07 22:56:09 -0500

    made transition styles for the list when added or deleted from list,data

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,11 +1,14 @@
 <script>
+  import { fade, scale } from "svelte/transition";
   import { FeedbackStore } from "../stores";
   import FeedbackItem from "./FeedbackItem.svelte";
 
 </script>
 
 {#each $FeedbackStore as fb (fb.id)}
+<div in:scale out:fade="{{ duration:500 }}" >
   <FeedbackItem item={fb} on:delete-feedback />
+</div>
 {/each}
 
 <style>
