042c8e2fafed2aa3176882851f18030c33a4473a
David <leanmachine42@yahoo.com>
2022-06-07 22:13:06 -0500

    created form and ratings components for user feedback

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,45 @@
 <script>
+import FeedbackForm from "./components/FeedbackForm.svelte";
+
   import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
 
   let feedback = [
-  {
-    id: 1,
-    rating: 10,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-  },
-  {
-    id: 2,
-    rating: 9,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-  },
-  {
-    id: 3,
-    rating: 8,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-  },
-]
+    {
+      id: 1,
+      rating: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+    },
+  ];
 
-const deleteFeedback = (e) => {
-  const itemId = e.detail;
-  feedback = feedback.filter(item => item.id !== itemId);
-}
+  $: count = feedback.length;
+  $: average = feedback.reduce(
+    (acc, { rating }, i, array) =>
+      i === array.length - 1 ? (acc + rating) / array.length : acc + rating,
+    0
+  );
 
+  const deleteFeedback = (e) => {
+    const itemId = e.detail;
+    feedback = feedback.filter((item) => item.id !== itemId);
+  };
 </script>
 
 <main class="container">
-  <FeedbackList 
-    {feedback}
-    on:delete-feedback={deleteFeedback}
-  />
+  <FeedbackForm />
+  <FeedbackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
 
 <style>
-
 </style>
