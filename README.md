445375bf8d71833113d4dcb2be11c945a4445421
David <leanmachine42@yahoo.com>
2022-06-07 21:05:55 -0500

    went through basic concepts of svelte with the app component

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Svelte exercise
 
 ## Documentation
-Documentation for the code base can be found <a href="">here</a>
+Documentation for the code base can be found <a href="https://www.youtube.com/watch?v=3TVy6GdtNuQ">here</a>
 
 ## Explanation
 Rating application using svelte and the basic usages and use cases
--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,50 @@
 <script>
-	export let name;
+	let firstName = "Kavooce";
+	let lastName = "King";
+	let color = "blue";
+	let showText = false;
+	let users = [
+		{
+			id:"1",
+			name:"John"
+		},
+		{
+			id:"2",
+			name:"Jane"
+		},
+		{
+			id:"3",
+			name:"Bob"
+		}
+	]
+
+	$: name = `${firstName} ${lastName}`;
+
+	const toggle = () => {
+		color = color === "blue" ? "red" : "blue";
+		showText = !showText;
+	}
+	const addToList = () => {
+		users = [...users,{ id:"4",name:"Mario" }]
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1 style="color:{color}">Hello {name}!</h1>
+	{#if showText}
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{/if}
+	{#if showText}
+	<p>Showing the text when true</p>
+	{:else}
+	<p>Showing this text on else</p>
+	{/if}
+	<button on:click={()=> color = "coral"} >Orange</button>
+	<button on:click={toggle} >Toggle</button>
+	<button on:click={addToList}>Add To List</button>
+	{#each users as user (user.id)}
+		<h3>{user.id}: {user.name}</h3>
+	{/each}
 </main>
 
 <style>
