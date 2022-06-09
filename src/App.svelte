e7bbe0887365c023f579bd4661ab898b656c6767
David <leanmachine42@yahoo.com>
2022-06-09 15:40:01 -0500

    created tabs, nav component

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
-import Footer from "./components/Footer.svelte";
-import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
+	import Header from "./components/Header.svelte";
+	import Tabs from "./components/Tabs.svelte";
+	import { PollTabs } from "./tslib/PollEnum";
 
+
+
+	let items = [PollTabs.CURRENT_POLLS, PollTabs.ADD_NEW_POLL];
+	let activeItem = PollTabs.CURRENT_POLLS;
+	
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
 
 </script>
 
 <Header />
 <main>
-
+	<Tabs {items} {activeItem} on:tabChange={tabChange} />
+	{#if activeItem === PollTabs.CURRENT_POLLS}
+		 <p>{ PollTabs.CURRENT_POLLS}</p>
+	{:else}
+		<p>{ PollTabs.ADD_NEW_POLL} </p>
+	{/if}
 </main>
 <Footer />
 
---
+++ src/tslib/PollEnum.ts
@@ -0,0 +1,5 @@
+
+export enum PollTabs {
+  CURRENT_POLLS = "Current Polls",
+  ADD_NEW_POLL = "Add New Poll"
+}
