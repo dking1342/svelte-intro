39dbe747b09b39ce5322f20f6cdc9ed850d6c128
David <leanmachine42@yahoo.com>
2022-06-09 15:14:03 -0500

    created header and footer components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<script>
+  // your script goes here
+</script>
+
+<footer>
+  <div class="copyright">Copyright &copy; 2020 Poll Svelte</div>
+</footer>
+
+<style>
+  footer {
+    padding:40px;
+    text-align: center;
+  }
+  .copyright {
+    color:#aaa;
+    font-size: 14px;
+    display: inline-block;
+    padding:20px;
+    border-top: 1px solid #ddd;
+  }
+</style>
+
+<!-- markup (zero or more items) goes here -->
---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+  // your script goes here
+</script>
+
+<header>
+  <h1>
+    <img src="./assets/images/logo.svg" alt="Poll Ninja Logo">
+  </h1>
+</header>
+
+<style>
+  header {
+    background-color: #f7f7f7;
+    padding:20px;
+  }
+  h1 {
+    text-align: center;
+  }
+  img {
+    max-width: 180px;
+  }
+</style>
+
+<!-- markup (zero or more items) goes here -->
