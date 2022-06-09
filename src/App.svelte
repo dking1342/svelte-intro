023a94e0812174177524b87c8ab5eb0b206bcdea
David <leanmachine42@yahoo.com>
2022-06-08 19:11:58 -0500

    reviewed reactive values

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,19 @@
 <script lang="ts">
-  let name = "Mario";
+  let firstName = "Mario";
+  let lastName = "Kart";
   let beltColor = "black"
 
-  const handleChange = () => {
-    beltColor = "orange";
-  }
-
-  const handleInput = (e) => {
-    beltColor = e.target.value;
+  $: fullName = `${firstName} ${lastName}`;
+  $: {
+    console.log(beltColor)
+    console.log(fullName)
   }
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p style="color:{beltColor}">{beltColor} belt</p>
-
-
-
-  <button on:click={handleChange}>Update belt color</button>
-  <!-- one way data bind -->
-  <!-- <input type="text" on:input={handleInput} value={beltColor}> -->
-  
-  <!-- two way data bind -->
+  <p>{fullName} - {beltColor} belt</p>
+  <input type="text" bind:value={firstName}>
+  <input type="text" bind:value={lastName}>
   <input type="text" bind:value={beltColor}>
 </main>
 
@@ -37,12 +29,12 @@
     margin: 0 auto;
   }
 
-  img {
+  /* img {
     height: 16rem;
     width: 16rem;
-  }
+  } */
 
-  h1 {
+  /* h1 {
     color: #ff3e00;
     text-transform: uppercase;
     font-size: 4rem;
@@ -50,7 +42,7 @@
     line-height: 1.1;
     margin: 2rem auto;
     max-width: 14rem;
-  }
+  } */
 
   p {
     max-width: 14rem;
@@ -58,13 +50,5 @@
     line-height: 1.35;
   }
 
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
 
-    p {
-      max-width: none;
-    }
-  }
 </style>
